0707bef183b2f2b17f66a73207d58b7aadf2d193
ebauer10 <60185189+ebauer10@users.noreply.github.com>
2020-03-02 09:32:37 -0600

    Create CalculateHandLen.py

---
+++ Week 4: Good Programming Practices Exceptions and Assertions/Problem Set 4/CalculateHandLen.py
@@ -0,0 +1,12 @@
+def calculateHandlen(hand):
+    """ 
+    Returns the length (number of letters) in the current hand.
+    
+    hand: dictionary (string-> int)
+    returns: integer
+    """
+    # TO DO... <-- Remove this comment when you code this function
+    handLen = 0
+    for num in hand.values():
+      handLen += num
+    return handLen
